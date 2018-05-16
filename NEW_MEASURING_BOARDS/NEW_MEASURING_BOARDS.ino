39676a7036c07d91e8ca126dc7df64eef536ce26
KLL <19226581+kllsamui@users.noreply.github.com>
2018-05-16 21:04:57 +0700

    Add files via upload

---
+++ NEW_MEASURING_BOARDS/NEW_MEASURING_BOARDS.ino
@@ -0,0 +1,26 @@
+//___________________________________________________ my usual setting
+const boolean dbp=true;        // false                 // false  here can disable diagnostic printout 
+#define db(t,x)     if(t) { Serial.print(x); }
+#define dbln(t,x)   if(t) { Serial.println(x); }
+                                                        // use: 
+                                                        // dbln(dbp,"test");
+
+//___________________________________________________ GLOBAL VARS
+float voltage = 0.00;
+float ampere = 0.00;
+
+//___________________________________________________ SETUP
+void setup() {
+    Serial.begin(115200);
+    while (!Serial){
+    ;                                                 // needed for leonardo
+    }
+    print_in_setup();
+    
+}
+
+//___________________________________________________ LOOP
+void loop() {
+    refreshReadings();
+    delay(5000);
+}
---
+++ NEW_MEASURING_BOARDS/Volt_Amp_Measurement.ino
@@ -0,0 +1,45 @@
+//___________________________________________________ measuring board voltage divider
+float Av = 0.0;
+float Bv =0.0244;
+//___________________________________________________ measuring board ACS712 HALL
+float Aoff= 512.0;                                    // MOD KLL for HALL
+float Ai = 0.0;
+float Bi = 0.07896;
+
+//___________________________________________________ show params at boot
+void print_in_setup(){
+  dbln(dbp,"Parameter Setup");
+  dbln(dbp,"VFinal = Av + Bv * VRaw(A0)");
+  db(dbp,"Av =: "); dbln(dbp,Av);
+  db(dbp,"Bv =: "); if (dbp) { Serial.println(Bv,4); }
+  
+  dbln(dbp,"IFinal = Ai + Bi * ( IRaw(A1)- Aoff )");
+  db(dbp,"Ai =: "); dbln(dbp,Ai);
+  db(dbp,"Bi =: "); if (dbp) { Serial.println(Bi,4); }
+  db(dbp,"Aoff =: "); dbln(dbp,Aoff);
+ 
+    }
+
+//___________________________________________________ read analog A0 VOLT ( via voltage divider )
+float getV() {
+  float VRaw = analogRead(A0);
+  float VFinal = Av + Bv * VRaw;
+  db(dbp," // VRaw: "); db(dbp,VRaw); db(dbp,", VFinal: "); db(dbp,VFinal);
+  return VFinal;
+}
+
+//___________________________________________________ read analog A1 AMP ( via ACS712 HALL )
+float getI() {
+  float IRaw = analogRead(A1);
+  float IFinal = Ai + Bi * (IRaw - Aoff);               // MOD KLL for HALL
+  db(dbp," // IRaw: "); db(dbp,IRaw); db(dbp,", IFinal: "); dbln(dbp,IFinal);
+  return IFinal;
+}
+
+//___________________________________________________ get measurements TO GLOBAL VARS
+void refreshReadings() {
+  db(dbp,"time: "); db(dbp,millis()/1000);
+  voltage = getV();
+  ampere  = getI();
+}
+
